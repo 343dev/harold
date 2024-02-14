2ab97d03221b35c06425536634d9557928daaa46
343dev <343dev@users.noreply.github.com>
2024-02-14 07:38:53 +0700

    Add description for props in .haroldrc.js

--- .haroldrc.js
+++ .haroldrc.js
@@ -1,12 +1,13 @@
+// projects without ESM has to export configuration via `module.exports = {}`
 export default {
   build: {
-    env: {
+    env: { // environment variables that need to be passed to the bundler
       NO_HASH: true,
     },
-    command: 'npm run build-production',
-    path: 'public',
+    command: 'npm run build-production', // project build command
+    path: 'public', // directory where the build files will take place
   },
-  categories: {
+  categories: { // file categories for comparison (RegExp)
     CSS: /\.css$/,
     JavaScript: /\.js$/,
     Images: /\.(png|jpe?g|gif|svg|webp|avif)$/,
